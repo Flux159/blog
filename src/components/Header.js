a39d9edfa29e0ac4b49d30bb3e6e30d652a78738
Flux159 <flux159@gmail.com>
2019-11-28 14:25:32 -0500

    Updating header links.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { StaticQuery, graphql } from "gatsby";
 import GitHubButton from "react-github-btn";
 import Link from "./link";
@@ -21,143 +21,163 @@ if (isSearchEnabled && config.header.search.indexName) {
 
 import Sidebar from "./sidebar";
 
+const Links = ({ githubUrl, helpUrl, tweetText, headerLinks, hiddenxs }) => {
+  const twitter = require("./images/twitter.svg");
+
+  const className = hiddenxs
+    ? "nav navbar-nav navBarUL navBarNav navbar-right navBarULRight hidden-xs"
+    : "nav navbar-nav navBarUL navBarNav navbar-right navBarULRight";
+
+  return (
+    <ul className={className}>
+      {headerLinks.map((link, key) => {
+        if (link.link !== "" && link.text !== "") {
+          return (
+            <li key={key}>
+              <a href={link.link} target="_blank">
+                {link.text}
+              </a>
+            </li>
+          );
+        }
+      })}
+      {helpUrl !== "" ? (
+        <li>
+          <a href={helpUrl}>
+            <img src={help} alt="Help icon" />
+          </a>
+        </li>
+      ) : null}
+      {tweetText !== "" || githubUrl !== "" ? (
+        <li className="divider hidden-xs"></li>
+      ) : null}
+      {tweetText !== "" ? (
+        <li>
+          <a
+            href={"https://twitter.com/intent/tweet?&text=" + tweetText}
+            target="_blank"
+          >
+            <img className="shareIcon" src={twitter} alt="Twitter" />
+          </a>
+        </li>
+      ) : null}
+      {githubUrl !== "" ? (
+        <li className="githubBtn">
+          <GitHubButton
+            href={githubUrl}
+            data-show-count="true"
+            aria-label="Star on GitHub"
+          >
+            Star
+          </GitHubButton>
+        </li>
+      ) : null}
+    </ul>
+  );
+};
+
 const Header = ({ location }) => {
   const [open, setOpen] = useState(false);
 
-  return (<StaticQuery
-    query={graphql`
-      query headerTitleQuery {
-        site {
-          siteMetadata {
-            headerTitle
-            githubUrl
-            helpUrl
-            tweetText
-            logo {
-              link
-              image
-            }
-            headerLinks {
-              link
-              text
+  return (
+    <StaticQuery
+      query={graphql`
+        query headerTitleQuery {
+          site {
+            siteMetadata {
+              headerTitle
+              githubUrl
+              helpUrl
+              tweetText
+              logo {
+                link
+                image
+              }
+              headerLinks {
+                link
+                text
+              }
             }
           }
         }
-      }
-    `}
-    render={data => {
-      const logoImg = require("./images/logo.svg");
-      const twitter = require("./images/twitter.svg");
-      const {
-        site: {
-          siteMetadata: {
-            headerTitle,
-            githubUrl,
-            helpUrl,
-            tweetText,
-            logo,
-            headerLinks
+      `}
+      render={data => {
+        const {
+          site: {
+            siteMetadata: {
+              headerTitle,
+              githubUrl,
+              helpUrl,
+              tweetText,
+              // logo,
+              headerLinks
+            }
           }
-        }
-      } = data;
-      return (
-        <div className="navBarWrapper">
-          <nav className="navbar navbar-default navBarDefault">
-            <div className="navbar-header navBarHeader">
-              <Link to="/" className="navbar-brand navBarBrand">
-                {/* {logo.image !== '' ?
+        } = data;
+        // const logoImg = require("./images/logo.svg");
+
+        return (
+          <div className="navBarWrapper">
+            <nav className="navbar navbar-default navBarDefault">
+              <div className="navbar-header navBarHeader">
+                <Link to="/" className="navbar-brand navBarBrand">
+                  {/* {logo.image !== '' ?
                   (<img className={'img-responsive'} src={logo.image} alt={'logo'} />)
                   :
                   (<img className={'img-responsive'} src={logoImg} alt={'logo'} />)
                 } */}
-                <div className="headerTitle">{headerTitle}</div>
-              </Link>
-              <button
-                type="button"
-                className="navbar-toggle collapsed navBarToggle"
-                onClick={() => setOpen(!open)}
+                  <div className="headerTitle">{headerTitle}</div>
+                </Link>
+                <button
+                  type="button"
+                  className="navbar-toggle collapsed navBarToggle"
+                  onClick={() => setOpen(!open)}
+                >
+                  <span className="sr-only">Toggle navigation</span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                </button>
+              </div>
+              {isSearchEnabled ? (
+                <div className="searchWrapper hidden-xs navBarUL">
+                  <Search collapse indices={searchIndices} />
+                </div>
+              ) : null}
+              <div
+                id="navbar"
+                className={
+                  open
+                    ? "navbar-collapse collapse show navBarCollapse"
+                    : "collapse navBarCollapse"
+                }
+                onClick={() => setOpen(false)}
               >
-                <span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-              </button>
-            </div>
-            {isSearchEnabled ? (
-              <div className="searchWrapper hidden-xs navBarUL">
-                <Search collapse indices={searchIndices} />
+                <div className="visible-xs">
+                  <Sidebar location={location} />
+                  {isSearchEnabled ? (
+                    <div>
+                      <hr />
+                      <div className="searchWrapper navBarUL">
+                        <Search collapse indices={searchIndices} />
+                      </div>
+                    </div>
+                  ) : null}
+                </div>
               </div>
-            ) : null}
-            <div
-              id="navbar"
-              className={open ? 'navbar-collapse collapse show navBarCollapse' : 'collapse navBarCollapse'}
-              onClick={() => setOpen(false)}
-            >
-              <div className="visible-xs">
-                <Sidebar location={location} />
-                <hr />
-                {isSearchEnabled ? (
-                  <div className="searchWrapper navBarUL">
-                    <Search collapse indices={searchIndices} />
-                  </div>
-                ) : null}
-              </div>
-              <ul className="nav navbar-nav navBarUL navBarNav navbar-right navBarULRight">
-                {headerLinks.map((link, key) => {
-                  if (link.link !== "" && link.text !== "") {
-                    return (
-                      <li key={key}>
-                        <a href={link.link} target="_blank">
-                          {link.text}
-                        </a>
-                      </li>
-                    );
-                  }
-                })}
-                {helpUrl !== "" ? (
-                  <li>
-                    <a href={helpUrl}>
-                      <img src={help} alt="Help icon" />
-                    </a>
-                  </li>
-                ) : null}
-                {tweetText !== "" || githubUrl !== "" ? (
-                  <li className="divider hidden-xs"></li>
-                ) : null}
-                {tweetText !== "" ? (
-                  <li>
-                    <a
-                      href={
-                        "https://twitter.com/intent/tweet?&text=" + tweetText
-                      }
-                      target="_blank"
-                    >
-                      <img
-                        className="shareIcon"
-                        src={twitter}
-                        alt="Twitter"
-                      />
-                    </a>
-                  </li>
-                ) : null}
-                {githubUrl !== "" ? (
-                  <li className="githubBtn">
-                    <GitHubButton
-                      href={githubUrl}
-                      data-show-count="true"
-                      aria-label="Star on GitHub"
-                    >
-                      Star
-                    </GitHubButton>
-                  </li>
-                ) : null}
-              </ul>
-            </div>
-          </nav>
-        </div>
-      );
-    }}
-  />)
-  };
+              <Links
+                githubUrl={githubUrl}
+                helpUrl={helpUrl}
+                tweetText={tweetText}
+                headerLinks={headerLinks}
+                hiddenxs={true}
+              />
+            </nav>
+          </div>
+        );
+      }}
+    />
+  );
+};
 
 export default Header;
